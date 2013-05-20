36329d83e6bd0447e94c28858eb72ec978b5a6cc
Wraithan (Chris McDonald) <xwraithanx@gmail.com>
2013-05-20 01:19:15 -0700

    update the github service to use irc-colors

--- github.js
+++ github.js
@@ -1,11 +1,9 @@
+var path = require('path')
+var colors = require('irc-colors')
 var api = require('zenircbot-api')
-var bot_config = api.load_config('../../bot.json')
-var zen = new api.ZenIRCBot(bot_config.redis.host,
-                            bot_config.redis.port,
-                            bot_config.redis.db)
+var zen = new api.ZenIRCBot()
 var sub = zen.get_redis_client()
-var color = require('./lib/colors')
-var github_config = api.load_config('./github.json')
+var github_config = api.load_config(path.join(__dirname, 'github.json'))
 
 
 zen.register_commands("github.js", [])
@@ -13,7 +11,7 @@ zen.register_commands("github.js", [])
 sub.subscribe('web_in')
 sub.on('message', function(channel, message){
     message = JSON.parse(message)
-    if (message.app != 'github') {
+    if (message.app !== 'github') {
         return null
     }
 
@@ -33,7 +31,7 @@ sub.on('message', function(channel, message){
             name_str = ''
         }
         message = (repo + ': ' + commit.id.substr(0,7) + ' *' +
-                   color.green + branch + color.reset + '* ' +
+                   colors.green(branch) + '* ' +
                    commit.message + name_str)
         zen.send_privmsg(irc_channel, message)
         console.log(channel + ' -> ' + message)
